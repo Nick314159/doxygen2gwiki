7bd9a968f8d365e0fff23fee548c2b40526621c1
andrewjwilkinson <andrewjwilkinson@cb0b4f5c-002e-0410-b718-03348fe08908>
2007-04-20 12:53:31 +0000

    Allow the user to select what labels to apply, if any.

--- src/doxygen2gwiki/options.py
+++ src/doxygen2gwiki/options.py
@@ -9,6 +9,10 @@ parser.add_option("-p", "--prefix", dest="prefix", default="Doxygen",
                   help="Use this prefix when generating wiki pages. (Default: Doxygen)")
 parser.add_option("-s", "--skip-svn", dest="skip_svn", default=False, action="store_true",
                   help="Skip attempts to control subversion. (Default: Control Subversion)")
+parser.add_option("-l", "--label", dest="labels", default=[], action="append",
+                  help="Apply this label to all pages.")
+parser.add_option("-n", "--no-labels", dest="no_labels", default=False, action="store_true",
+                  help="Don't apply any labels to the pages. (Overrides any --label options)")
 
 (options, args) = parser.parse_args()
 
